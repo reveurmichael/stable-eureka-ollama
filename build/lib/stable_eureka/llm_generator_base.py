909f5a9f7f6f05f46c007b1e27697d8e42ed571c
lunde chen <lunde.chen@outlook.com>
2024-08-30 20:11:22 +0800

    s

--- build/lib/stable_eureka/llm_generator_base.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-
-
-class LLMGeneratorBase(ABC):
-    def __init__(self, model: str):
-        self.model = model
-
-    def generate(self, **kwargs) -> list[str]:
-        raise NotImplementedError("LLM generate method is not implemented")
