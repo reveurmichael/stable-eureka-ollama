37550adf58be4ae1e9352641bb648684edea8751
lunde chen <lunde.chen@outlook.com>
2024-09-04 14:51:54 +0800

    s

--- my_main_001.py
+++ my_main_001.py
@@ -2,8 +2,5 @@ from stable_eureka import MyStableEurekaForCheckingEnvPy
 
 
 if __name__ == '__main__':
-    m_checker = MyStableEurekaForCheckingEnvPy(config_path='./configs/bipedal_walker_ollama.yml', 
-                               experiment_datetime='2024-08-20-07-57',
-                               iteration=0,
-                               sample=2)
+    m_checker = MyStableEurekaForCheckingEnvPy(config_path='./configs/bipedal_walker_ollama.yml', experiment_datetime='2024-08-20-07-57', iteration=0, sample=2)
     m_checker.run()
