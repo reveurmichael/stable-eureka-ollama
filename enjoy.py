0d98c8b162e091ef642ca26124e7bc92bdf7c395
lunde chen <lunde.chen@outlook.com>
2024-09-04 10:34:28 +0800

    a

--- enjoy.py
+++ enjoy.py
@@ -10,7 +10,7 @@ from huggingface_sb3 import EnvironmentName
 from stable_baselines3.common.callbacks import tqdm
 from stable_baselines3.common.utils import set_random_seed
 
-import rl_zoo3.import_envs  # noqa: F401 pylint: disable=unused-import
+import rl_zoo3.import_envs  
 from rl_zoo3 import ALGOS, create_test_env, get_saved_hyperparams
 from rl_zoo3.exp_manager import ExperimentManager
 from rl_zoo3.load_from_hub import download_from_hub
